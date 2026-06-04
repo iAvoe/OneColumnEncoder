c4521d7e647a06996b790f5c290f50c7344df57b
iAvoe <jgong117398@gmail.com>
2026-06-04 11:27:05 -0400

    Implemented SplitTextCard mvvm

---
+++ Components/Cards/SplitTextCard.xaml.cs
@@ -0,0 +1,66 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace OneColumnEncoder.Components.Cards
+{
+    public partial class SplitTextCard : UserControl
+    {
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register(nameof(Title), typeof(string), typeof(SplitTextCard), new PropertyMetadata(string.Empty));
+
+        public static readonly DependencyProperty LeftTextProperty =
+            DependencyProperty.Register(
+                nameof(LeftText),
+                typeof(string),
+                typeof(SplitTextCard),
+                new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static readonly DependencyProperty RightTextProperty =
+            DependencyProperty.Register(
+                nameof(RightText),
+                typeof(string),
+                typeof(SplitTextCard),
+                new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public static readonly DependencyProperty IsReadOnlyProperty =
+            DependencyProperty.Register(nameof(IsReadOnly), typeof(bool), typeof(SplitTextCard), new PropertyMetadata(true));
+
+        public static readonly DependencyProperty TextBoxMinHeightProperty =
+            DependencyProperty.Register(nameof(TextBoxMinHeight), typeof(double), typeof(SplitTextCard), new PropertyMetadata(120.0));
+
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        public string LeftText
+        {
+            get => (string)GetValue(LeftTextProperty);
+            set => SetValue(LeftTextProperty, value);
+        }
+
+        public string RightText
+        {
+            get => (string)GetValue(RightTextProperty);
+            set => SetValue(RightTextProperty, value);
+        }
+
+        public bool IsReadOnly
+        {
+            get => (bool)GetValue(IsReadOnlyProperty);
+            set => SetValue(IsReadOnlyProperty, value);
+        }
+
+        public double TextBoxMinHeight
+        {
+            get => (double)GetValue(TextBoxMinHeightProperty);
+            set => SetValue(TextBoxMinHeightProperty, value);
+        }
+
+        public SplitTextCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
