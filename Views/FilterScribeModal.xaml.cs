87f72925a241606258e89fffaea41d26c25e0fcc
iAvoe <jgong117398@gmail.com>
2026-06-14 21:42:31 -0400

    Implemented line break input restriction in ffmpeg filter textbox

--- Views/FilterScribeModal.xaml.cs
+++ Views/FilterScribeModal.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using OneColumnEncoder.Helpers;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -19,6 +20,31 @@ namespace OneColumnEncoder.Views
             double newSize = box.FontSize + (e.Delta > 0 ? 1 : -1);
             box.FontSize = double.Clamp(newSize, 8, 48);
             e.Handled = true;
+        }
+
+        private void FfmpegFreeText_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter && e.Key != Key.Return)
+                return;
+
+            e.Handled = true;
+        }
+
+        private void FfmpegFreeText_Pasting(object sender, DataObjectPastingEventArgs e)
+        {
+            string? text = e.SourceDataObject.GetData(DataFormats.UnicodeText) as string
+                ?? e.SourceDataObject.GetData(DataFormats.Text) as string;
+
+            if (text is null)
+                return;
+
+            string normalized = text.Replace("\r", string.Empty).Replace("\n", string.Empty);
+            if (normalized == text)
+                return;
+
+            DataObject dataObject = new DataObject();
+            dataObject.SetData(DataFormats.UnicodeText, normalized);
+            e.DataObject = dataObject;
         }
     }
 }
