e4d07ea5ee89c0fd9037ca53200d4e5bfdff038f
iAvoe <jgong117398@gmail.com>
2026-06-20 06:13:29 -0400

    Separate queue sidebar to dedicated component

---
+++ Components/QueueSidebarPanel.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace OneColumnEncoder.Components
+{
+    public partial class QueueSidebarPanel : UserControl
+    {
+        public QueueSidebarPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
