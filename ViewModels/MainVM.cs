4603fe418abf038d78373129cc2ea82fbb2c64e2
iAvoe <jgong117398@gmail.com>
2026-06-20 19:48:41 -0400

    Suppress finalize compiler note

--- ViewModels/MainVM.cs
+++ ViewModels/MainVM.cs
@@ -1975,6 +1975,7 @@ namespace OneColumnEncoder.ViewModels
             UnsubFromImportedToolZones();
             UnsubFromToolsChecklist();
             base.Dispose();
+            GC.SuppressFinalize(this);
         }
     }
 }
