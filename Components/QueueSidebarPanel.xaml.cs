b243fd628b2a40d41148d0ff566092ab68288536
iAvoe <jgong117398@gmail.com>
2026-06-21 08:39:55 -0400

    Implemented QueueSidebarPanel mouse wheel scrolling

--- Components/QueueSidebarPanel.xaml.cs
+++ Components/QueueSidebarPanel.xaml.cs
@@ -1,4 +1,6 @@
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace OneColumnEncoder.Components
 {
@@ -7,6 +9,36 @@ namespace OneColumnEncoder.Components
         public QueueSidebarPanel()
         {
             InitializeComponent();
+
+            foreach (var listBox in new[] { WaitingListBox, UnfinishedListBox, CompletedListBox })
+            {
+                listBox.PreviewMouseWheel += ListBox_PreviewMouseWheel;
+            }
+        }
+
+        private void QueueSidebarPanel_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ForwardWheelToScrollViewer(e);
+        }
+
+        private void ListBox_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ForwardWheelToScrollViewer(e);
+        }
+
+        private void ForwardWheelToScrollViewer(MouseWheelEventArgs e)
+        {
+            if (e.Handled)
+                return;
+
+            double newOffset = QueueScrollViewer.VerticalOffset - e.Delta;
+            if (newOffset < 0)
+                newOffset = 0;
+            else if (newOffset > QueueScrollViewer.ScrollableHeight)
+                newOffset = QueueScrollViewer.ScrollableHeight;
+
+            QueueScrollViewer.ScrollToVerticalOffset(newOffset);
+            e.Handled = true;
         }
     }
 }
