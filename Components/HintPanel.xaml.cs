578a18836c77b2ed3a3191544c92f5781c2a952a
iAvoe <jgong117398@gmail.com>
2026-06-26 14:51:41 -0400

    Added opt-in HintPanel language switching in main page

--- Components/HintPanel.xaml.cs
+++ Components/HintPanel.xaml.cs
@@ -1,5 +1,7 @@
+using OneColumnEncoder.Models;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 
@@ -11,6 +13,10 @@ namespace OneColumnEncoder.Components
             DependencyProperty.Register(nameof(Text), typeof(string), typeof(HintPanel),
                 new PropertyMetadata(null));
 
+        public static readonly DependencyProperty RefreshTextOnLanguageChangedProperty =
+            DependencyProperty.Register(nameof(RefreshTextOnLanguageChanged), typeof(bool), typeof(HintPanel),
+                new PropertyMetadata(false, OnRefreshTextOnLanguageChangedChanged));
+
         public static readonly DependencyProperty HintBrushProperty =
             DependencyProperty.Register(nameof(HintBrush), typeof(Brush), typeof(HintPanel),
                 new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender));
@@ -19,6 +25,12 @@ namespace OneColumnEncoder.Components
         {
             get => (string)GetValue(TextProperty);
             set => SetValue(TextProperty, value);
+        }
+
+        public bool RefreshTextOnLanguageChanged
+        {
+            get => (bool)GetValue(RefreshTextOnLanguageChangedProperty);
+            set => SetValue(RefreshTextOnLanguageChangedProperty, value);
         }
 
         public Brush HintBrush
@@ -30,9 +42,40 @@ namespace OneColumnEncoder.Components
         public HintPanel()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+            Unloaded += OnUnloaded;
             FontSize = 9.0;
             if (ReadLocalValue(HintBrushProperty) == DependencyProperty.UnsetValue)
                 SetCurrentValue(HintBrushProperty, TryFindResource("GlobalTertiary") as Brush);
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            SyncLanguageChangedSubscription();
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            UILangProviderM.CurrentChanged -= OnLanguageChanged;
+        }
+
+        private static void OnRefreshTextOnLanguageChangedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            HintPanel hintPanel = (HintPanel)d;
+            hintPanel.SyncLanguageChangedSubscription();
+        }
+
+        private void SyncLanguageChangedSubscription()
+        {
+            UILangProviderM.CurrentChanged -= OnLanguageChanged;
+            if (RefreshTextOnLanguageChanged)
+                UILangProviderM.CurrentChanged += OnLanguageChanged;
+        }
+
+        private void OnLanguageChanged()
+        {
+            BindingExpression? binding = GetBindingExpression(TextProperty);
+            binding?.UpdateTarget();
         }
 
         private void CopyText_Executed(object sender, ExecutedRoutedEventArgs e)
