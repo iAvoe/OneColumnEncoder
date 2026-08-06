a0ae87ee34860ed8191c4a34e5aee8cc61706dcc
iAvoe <jgong117398@gmail.com>
2026-08-05 23:58:25 -0400

    Refined repart mode EncodingMonitor (sidebar) layout

--- Components/RepartOutputSidebarPanel.xaml.cs
+++ Components/RepartOutputSidebarPanel.xaml.cs
@@ -5,15 +5,44 @@ namespace OneColumnEncoder.Components;
 
 public partial class RepartOutputSidebarPanel : UserControl
 {
-    public RepartOutputSidebarPanel() => InitializeComponent();
+    public RepartOutputSidebarPanel()
+    {
+        InitializeComponent();
+
+        foreach (var listBox in new[] { WaitingListBox, UnfinishedListBox, CompletedListBox })
+        {
+            listBox.PreviewMouseWheel += ListBox_PreviewMouseWheel;
+        }
+    }
 
     private void Sidebar_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
     {
-        if (e.Handled) return;
-        SidebarScrollViewer.ScrollToVerticalOffset(Math.Clamp(
-            SidebarScrollViewer.VerticalOffset - e.Delta,
-            0,
-            SidebarScrollViewer.ScrollableHeight));
+        ForwardWheelToScrollViewer(e);
+    }
+
+    private void ListBox_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+    {
+        ForwardWheelToScrollViewer(e);
+    }
+
+    private void ForwardWheelToScrollViewer(MouseWheelEventArgs e)
+    {
+        if (e.Handled)
+        {
+            return;
+        }
+
+        var newOffset = SidebarScrollViewer.VerticalOffset - e.Delta;
+        if (newOffset < 0)
+        {
+            newOffset = 0;
+        }
+        else if (newOffset > SidebarScrollViewer.ScrollableHeight)
+        {
+            newOffset = SidebarScrollViewer.ScrollableHeight;
+        }
+
+        SidebarScrollViewer.ScrollToVerticalOffset(newOffset);
         e.Handled = true;
     }
 }
